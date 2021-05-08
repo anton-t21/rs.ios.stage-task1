105455033cbc264b2c719e1f3c909c6b2c8a2648
AT <libra.giocondo@gmail.com>
2021-05-08 16:15:17 +0300

    feat: execute exercise 5

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
@@ -10,7 +10,43 @@
 
 - (NSArray <NSNumber *> *)trackLeaderBoard:(NSArray <NSNumber *> *)rankedArray
                                playerArray:(NSArray <NSNumber *> *)playerArray {
-    return @[];
+
+    NSMutableArray *resultArray = [NSMutableArray new];
+
+    NSOrderedSet *orderedSet = [NSOrderedSet orderedSetWithArray:rankedArray];
+    NSArray *rankedArrayWithoutDuplicates = [orderedSet array];
+
+    int numberOfElements = (int)(rankedArrayWithoutDuplicates.count - 1);
+
+    for (NSNumber *player in playerArray) {
+        int realIndex = 0;
+        if (numberOfElements >= 0) {
+            for (NSNumber *ranked in rankedArrayWithoutDuplicates) {
+                if (realIndex == numberOfElements) {
+                    if (player.intValue >= ranked.intValue) {
+                        NSNumber *rank = [NSNumber numberWithInt:realIndex + 1];
+                        [resultArray addObject:rank];
+                        break;
+                    } else {
+                        NSNumber *rank = [NSNumber numberWithInt:realIndex + 2];
+                        [resultArray addObject:rank];
+                        break;
+                    }
+                } else {
+                    if (player.intValue >= ranked.intValue) {
+                        NSNumber *rank = [NSNumber numberWithInt:realIndex + 1];
+                        [resultArray addObject:rank];
+                        break;
+                    }
+                }
+                realIndex++;
+            }
+        } else {
+            NSNumber *rank = [NSNumber numberWithInt:1];
+            [resultArray addObject:rank];
+        }
+    }
+    return resultArray;
 }
 
 @end
